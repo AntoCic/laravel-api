a2b32a1e3bd643708396c271f2bde9a1682867b9
AntoCic <anto.cic.127@gmail.com>
2024-06-12 17:22:36 +0200

    First commit

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::orderBy('name', 'asc')->with('type', 'technologies')->paginate(9);
+        return response()->json([
+            'results' => $projects
+        ]);
+    }
+}
